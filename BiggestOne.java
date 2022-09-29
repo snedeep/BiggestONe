34cd7da6fb124f05ba49465bbf96270544d49ff9
SandeepMadiwal <111920238+snedeep@users.noreply.github.com>
2022-09-29 11:40:49 +0530

    Add files via upload

---
+++ BiggestOne.java
@@ -0,0 +1,17 @@
+package pro430;
+import java.util.*;
+public class BiggestOne {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if (a>b&&a>c) {
+			System.out.println("biggest"+a);
+		else if(b>c)
+			System.out.println(b);
+		else 
+			System.out.println(c);}
+	}
+
+}
